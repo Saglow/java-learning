cc4bf85917c23333fe838238df90706fce7b7bb0
fangyuhui <fangyuhui2001@gmail.com>
2023-07-20 16:50:07 +0800

    gpt给出的GBK写法好像不太对

---
+++ src/main/java/cn/saglow/java/exercise/day2/UTF8HelloAndGBKHello.java
@@ -0,0 +1,56 @@
+package cn.saglow.java.exercise.day2;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Description: Utf8保存你好
+ * Author: HW
+ * Date: 2023/7/20
+ */
+public class UTF8HelloAndGBKHello {
+    public static void main(String[] args) {
+        String filePathU = "D:/workspace/temp/UTF_8.txt";
+        String filePathG = "D:/workspace/temp/GBK.txt";
+
+        File fileU = new File(filePathU);
+        try {
+            fileU.getParentFile().mkdirs();
+            fileU.createNewFile();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        File fileG = new File(filePathG);
+        try {
+            fileG.getParentFile().mkdirs();
+            fileG.createNewFile();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            OutputStream outputStream = new FileOutputStream(filePathU);
+            // 此时，outputStream 就是用来写入文件的流
+            outputStream.write("你好".getBytes(StandardCharsets.UTF_8));
+            outputStream.close(); // 关闭流
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            OutputStream outputStream = new FileOutputStream(filePathG);
+            // 此时，outputStream 就是用来写入文件的流
+            String contentToWrite="你好";
+            byte[] contentInGBK = contentToWrite.getBytes("GBK");
+            outputStream.write(contentInGBK);
+            outputStream.close(); // 关闭流
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
