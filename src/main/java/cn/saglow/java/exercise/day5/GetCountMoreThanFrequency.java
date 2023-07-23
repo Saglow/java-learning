abc809aa09756ae108d12ce6fb65965f43618262
fangyuhui <fangyuhui2001@gmail.com>
2023-07-23 10:33:17 +0800

    递归查找特定后缀文件

---
+++ src/main/java/cn/saglow/java/exercise/day5/GetCountMoreThanFrequency.java
@@ -0,0 +1,45 @@
+package cn.saglow.java.exercise.day5;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Description: 获得出现次数大于Frequency的字符串
+ * Author: HW
+ * Date: 2023/7/23
+ */
+public class GetCountMoreThanFrequency {
+    public static List<String> getCountMoreThenMaxNumber(List<Set<String>> content, Integer maxNumber) {
+        return content.stream()
+                .flatMap(Set::stream)
+                .toList()
+                .stream()
+                .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(i->i.getValue()>maxNumber)
+                .collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue))
+                .keySet()
+                .stream()
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<Set<String>> list = new ArrayList<>();
+        Set<String> set = Set.of("A","B","C");
+        Set<String> set1 = Set.of("A","B","C","D");
+        Set<String> set2= Set.of("A","B","C","D","E");
+        Set<String> set3 = Set.of("A","B","C","D","E","F");
+        Set<String> set4 = Set.of("A","B","C","D","E","F","G");
+        Set<String> set5 = Set.of("A","B","C","D","E","F","G","H");
+        list.add(set);
+        list.add(set1);
+        list.add(set2);
+        list.add(set3);
+        list.add(set4);
+        list.add(set5);
+        System.out.println(list);
+        System.out.println(getCountMoreThenMaxNumber(list,3));
+    }
+}
