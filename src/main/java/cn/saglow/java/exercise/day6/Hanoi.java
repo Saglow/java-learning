fecfd3e8fe6f55c51f24cb58441c3eebb3b169c8
fangyuhui <fangyuhui2001@gmail.com>
2023-07-24 15:27:59 +0800

    一开始写了个无限递归，值得反思

---
+++ src/main/java/cn/saglow/java/exercise/day6/Hanoi.java
@@ -0,0 +1,27 @@
+package cn.saglow.java.exercise.day6;
+
+/**
+ * Description: 汉诺塔
+ * Author: HW
+ * Date: 2023/7/24
+ */
+public class Hanoi {
+    public static void printHanoiStep(String source, String target, String temp, Integer depth) {
+        if(depth==1){
+            System.out.println("Move 1 from "+source+" to "+target);
+            return;
+            //一开始写了个无限递归，值得反思
+        }
+        printHanoiStep(source,temp,target,depth-1);
+        System.out.println("Move " + depth + " from " + source + " to " + target);
+        printHanoiStep(temp,target,source,depth-1);
+    }
+
+    public static void main(String[] args) {
+        String source = "A";
+        String temp = "B";
+        String target = "C";
+        int depth = 3;
+        printHanoiStep(source, target, temp, depth);
+    }
+}
